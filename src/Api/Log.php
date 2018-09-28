0e677606ac83bcef2ae1b9f77f88cf264a2f57b2
voltan <djvoltan@gmail.com>
2018-09-28 10:00:53 +0330

    Update to bs4

--- src/Api/Log.php
+++ src/Api/Log.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Pi Engine (http://pialog.org)
+ * Pi Engine (http://piengine.org)
  *
- * @link            http://code.pialog.org for the Pi Engine source repository
- * @copyright       Copyright (c) Pi Engine http://pialog.org
- * @license         http://pialog.org/license.txt New BSD License
+ * @link            http://code.piengine.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://piengine.org
+ * @license         http://piengine.org/license.txt New BSD License
  */
 
 /**
@@ -52,4 +52,4 @@ class Log extends AbstractApi
         $row->ip          = Pi::user()->getIp();
         $row->save();
     }
-}	
+}
